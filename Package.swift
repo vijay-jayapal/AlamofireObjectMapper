88c5401c10f0471a294b2da58aa5f7fa6d1bf9df
Tom Brow <tom@tombrow.com>
2021-10-15 15:50:22 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,6 +24,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AlamofireObjectMapper",
-            path: "./AlamofireObjectMapper"),
+            dependencies: ["Alamofire", "ObjectMapper"]),
     ]
 )
