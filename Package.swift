54eb90209aa2ba9384a5dec467748dc59a1dc2bc
Tom Brow <tom@tombrow.com>
2021-10-20 10:59:11 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 
-// swift-tools-version:5.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "AlamofireObjectMapper",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0"),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.3.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "3.5.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +25,8 @@ let package = Package(
         .target(
             name: "AlamofireObjectMapper",
             dependencies: ["Alamofire", "ObjectMapper"],
-            path: "./AlamofireObjectMapper"
+            path: "./AlamofireObjectMapper",
+            exclude: ["Info.plist"]
         ),
     ]
 )
